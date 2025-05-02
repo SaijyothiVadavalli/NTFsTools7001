60f5acb45412a25353491670f375a94cd366343d
saijyothiVadavalli <jyothisai015@gmail.com>
2025-05-02 12:33:48 +0530

    TL reviewed NB code

--- GITProj01/src/test/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/test/java/com/nt/nb/NetBankingService.java
@@ -1,5 +1,5 @@
 package com.nt.nb;
-
+//performing  RTGS payment
 public class NetBankingService {
 public String transforMoney(long srcAccount,long destAccount,String ifscCode,double amount) {
 	return amount+"is transfering from "+srcAccount+"to"+destAccount;
