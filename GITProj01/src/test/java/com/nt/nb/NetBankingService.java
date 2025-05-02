bb41213e511c88f7e14e2ef4b40d8abececeff89
saijyothiVadavalli <jyothisai015@gmail.com>
2025-05-02 14:25:46 +0530

    Dev1 completed IMPS

--- GITProj01/src/test/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/test/java/com/nt/nb/NetBankingService.java
@@ -3,6 +3,10 @@ package com.nt.nb;
 public class NetBankingService {
 public String transforMoney(long srcAccount,long destAccount,String ifscCode,double amount) {
 	return amount+"is transfering from "+srcAccount+"to"+destAccount;
+	}
+public String transforMoneyUsingIMPS(long srcAccount,long destAccount,String ifscCode,double amount) {
+	return amount+"is transfering from "+srcAccount+"to"+destAccount+"Using IMPS";
+
 	
 }
 }
