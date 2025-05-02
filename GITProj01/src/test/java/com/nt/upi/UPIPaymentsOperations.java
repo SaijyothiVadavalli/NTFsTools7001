e4bb44c4074b0e368d88f1523992d1518d56e303
saijyothiVadavalli <jyothisai015@gmail.com>
2025-05-02 10:43:08 +0530

    Dev1- completed UPI Payment feature

---
+++ GITProj01/src/test/java/com/nt/upi/UPIPaymentsOperations.java
@@ -0,0 +1,8 @@
+package com.nt.upi;
+
+public class UPIPaymentsOperations {
+public String doPayment(long phNo,double amount) {
+	return amount+"amount is paid to phone number::"+phNo;
+	
+}
+}
---
+++ GITProj01/src/test/java/nit/GITProj01/App.java
@@ -0,0 +1,5 @@
+package nit.GITProj01;
+
+public class App {
+
+}
